69d1715088525edb09c6ca3f4191d846ae69dac8
Gerald <gerald@blackberryhill.net>
2023-04-03 20:59:17 -0500

    Updated Index.html, and JS.

---
+++ script.js
@@ -0,0 +1,9 @@
+
+getApi();
+function getApi() {
+    var newUrlCity = 'api.openweathermap.org/data/2.5/forecast?lat=37.129&lon=-84.0833&appid={API key}'
+
+    fetch(newUrlCity)
+        
+}
+console.log(newUrlCity);
